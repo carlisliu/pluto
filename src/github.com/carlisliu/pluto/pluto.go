759de351deb3bb06347ceba91ddb92b44adf556c
carlisliu <carlisliu@gmail.com>
2018-01-02 21:08:47 +0800

    add newApp

--- src/github.com/carlisliu/pluto/pluto.go
+++ src/github.com/carlisliu/pluto/pluto.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/basicauth"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
+	"time"
 )
 
 func main() {
@@ -11,12 +13,36 @@ func main() {
 
 	app.Logger().SetLevel("debug")
 
+	authConfig := basicauth.Config{
+		Users:   map[string]string{"myusername": "mypassword", "name": "password"},
+		Realm:   "Auth Required",
+		Expires: time.Duration(30) * time.Minute,
+	}
+
+	authentication := basicauth.New(authConfig)
+
 	app.Use(recover.New())
 	app.Use(logger.New())
+
+	needAuth := app.Party("/admin", authentication)
+
+	needAuth.Get("/", h)
+	needAuth.Get("/profile", h)
+	needAuth.Get("/settings", h)
 
 	router(app)
 
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
+
+func newApp() *iris.Application {
+	app := iris.New()
+
+	app.Get("/", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"Status": "OK"})
+	})
+
+	return app
 }
 
 func router(app *iris.Application) {
@@ -31,4 +57,9 @@ func router(app *iris.Application) {
 	app.Get("/hello", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"Message": "Hello iris!"})
 	})
+}
+
+func h(ctx iris.Context) {
+	username, password, _ := ctx.Request().BasicAuth()
+	ctx.Writef("%s %s:%s", ctx.Path(), username, password)
 }
